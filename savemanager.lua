af64382cd321040e2b13c638e73be74a2373ff42
ErethonRunsU <erethon1@gmail.com>
2024-06-25 21:41:48 +0200

    Update savemanager.lua

--- savemanager.lua
+++ savemanager.lua
@@ -287,7 +287,7 @@ local SaveManager = {} do
 
 			local httprequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
 			local BrowserLIB = loadstring(game:HttpGet("https://raw.githubusercontent.com/ErethonRunsU/EvolutionSkid/main/browserlib.lua"))()
-			local Configs = game:GetService("HttpService"):JSONDecode(httprequest({Url = 'https://raw.githubusercontent.com/laagginq/Evolution/main/configs.json'}).Body)['Configs']
+			local Configs = game:GetService("HttpService"):JSONDecode(httprequest({Url = 'https://raw.githubusercontent.com/ErethonRunsU/EvolutionSkid/main/configs.json'}).Body)['Configs']
 			local Browser = BrowserLIB:Create(false)
 
 			for i,v in ipairs(Configs) do 
